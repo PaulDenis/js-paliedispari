818c216580d9e0a7adada929c2694699a1c54767
PaulDenis <d.paul@outlook.it>
2021-05-03 19:50:45 +0200

    Inizio Esercizio

---
+++ Esercizio_1/script/script.js
@@ -0,0 +1,6 @@
+// 1 - Palindroma
+// Chiedere all'utente di inserire una parola
+
+var word = prompt("Inserisci la tua parola");
+
+// Creare una funzione per capire se la parola inserita è palindroma
