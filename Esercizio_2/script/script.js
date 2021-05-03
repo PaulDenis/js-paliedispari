25710a89e99c19739cb9526bf47d72bb1053319b
PaulDenis <d.paul@outlook.it>
2021-05-03 22:14:47 +0200

    Aggiunta di stile.

--- Esercizio_2/script/script.js
+++ Esercizio_2/script/script.js
@@ -2,7 +2,9 @@
 
 // L'utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 var pariDispari = prompt("Pari o dispari?").toLowerCase();
-var playerNum = prompt("Scegli un numero da 1 a 5!");
+do {
+    var playerNum = prompt("Scegli un numero da 1 a 5!");
+} while (playerNum <1 || playerNum > 5);
 console.log(pariDispari);
 console.log(playerNum);
 
@@ -16,7 +18,18 @@ console.log(sum);
 
 // Dichiariamo chi ha vinto.
 if (pariDispari == "pari" && isEven(sum) || pariDispari == "dispari" && isEven(sum) == false) {
-    console.log("Hai vinto");
+   var win = "Hai vinto";
+   document.getElementById("sum").innerHTML = "La somma è <span>" + sum + "</span>. Di conseguenza:";
+   document.getElementById("result").innerHTML = win;
+
 } else {
-    console.log("Hai perso");
+    var lose = "Hai perso";
+    document.getElementById("sum").innerHTML = "La somma è <span>" + sum + "</span>. Di conseguenza:";
+    document.getElementById("result").innerHTML = lose;
 }
+
+//innerHTML
+
+document.getElementById("pariDispari").innerHTML = "Hai scelto di essere: " + "<span>" + pariDispari + "</span>";
+document.getElementById("player_num").innerHTML = "Hai scelto il numero: " + "<span>" + playerNum + "</span>";
+document.getElementById("pc_num").innerHTML = "Il pc ha scelto il numero: " + "<span>" + pcNum + "</span>";
